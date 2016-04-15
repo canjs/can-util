53d2f377f4ebf14d80a8958f6026885dd6f9e4eb
Jan Jorgensen <jandjorgensen@gmail.com>
2016-04-15 14:08:40 -0700

    assign test

--- js/assign/assign_test.js
+++ js/assign/assign_test.js
@@ -1,8 +1,15 @@
-var cid = require('can-util/js/assign/');
+var assign = require('can-util/js/assign/');
 var QUnit = require('steal-qunit');
 
 QUnit.module("can-util/js/assign")
 
-QUnit.test("basics", function(){
-	ok(false);
+QUnit.test("Assign all properties to an object", function(){
+	var a = 		{a: 1, b: 2, 	   d: 3},
+		b = 		{a: 1, b: 3, c: 2}
+		expected =  {a: 1, b: 3, c: 2, d: 3},
+		actual = assign(a, b);
+
+	for (var prop in actual){
+		equal(expected[prop], actual[prop]);
+	}
 });
