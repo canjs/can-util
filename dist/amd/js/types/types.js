24528c1b758b5f8532058b23c6305c6d036c393d
Justin Meyer <justinbmeyer@gmail.com>
2016-04-24 13:29:50 -0500

    3.0.0-pre.4

---
+++ dist/amd/js/types/types.js
@@ -0,0 +1,22 @@
+/*can-util@3.0.0-pre.3#js/types/types*/
+define(function (require, exports, module) {
+    var isPromise = require('../is-promise/is-promise');
+    var types = {
+        isMapLike: function () {
+            return false;
+        },
+        isListLike: function () {
+            return false;
+        },
+        isPromise: function (obj) {
+            return isPromise(obj);
+        },
+        isConstructor: function () {
+            return false;
+        },
+        isCallableForValue: function (obj) {
+            return typeof obj === 'function' && !types.isConstructor(obj);
+        }
+    };
+    module.exports = types;
+});
---
+++ dist/cjs/js/is-web-worker/is-web-worker.js
@@ -0,0 +1,4 @@
+/*can-util@3.0.0-pre.3#js/is-web-worker/is-web-worker*/
+module.exports = function () {
+    return typeof WorkerGlobalScope !== 'undefined' && this instanceof WorkerGlobalScope;
+};
