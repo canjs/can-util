ed7923f9c61ada85c1bf9dc1a374773854ec671b
Justin Meyer <justinbmeyer@gmail.com>
2016-04-25 15:06:43 -0500

    making strings return true for https://github.com/canjs/canjs/issues/2378

--- js/is-array-like/is-array-like-test.js
+++ js/is-array-like/is-array-like-test.js
@@ -6,3 +6,8 @@ QUnit.module("can-util/js/is-array-like");
 QUnit.test("basics", function(){
 	ok(isArrayLike({0: 1, length: 1}));
 });
+
+
+QUnit.test("string", function(){
+	ok(isArrayLike("yes"));
+});
