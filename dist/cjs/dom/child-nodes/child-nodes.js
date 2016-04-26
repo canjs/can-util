116d488986501d4548ef64b8f3875bdc95ef7818
Justin Meyer <justinbmeyer@gmail.com>
2016-04-26 13:13:52 -0500

    3.0.0-pre.9

---
+++ dist/cjs/dom/child-nodes/child-nodes.js
@@ -0,0 +1,16 @@
+/*can-util@3.0.0-pre.8#dom/child-nodes/child-nodes*/
+function childNodes(node) {
+    var childNodes = node.childNodes;
+    if ('length' in childNodes) {
+        return childNodes;
+    } else {
+        var cur = node.firstChild;
+        var nodes = [];
+        while (cur) {
+            nodes.push(cur);
+            cur = cur.nextSibling;
+        }
+        return nodes;
+    }
+}
+module.exports = childNodes;
---
+++ dist/cjs/js/each/each.js
@@ -0,0 +1,23 @@
+/*can-util@3.0.0-pre.8#js/each/each*/
+var isArrayLike = require('../is-array-like/is-array-like.js');
+function each(elements, callback, context) {
+    var i = 0, key, len, item;
+    if (elements) {
+        if (isArrayLike(elements)) {
+            for (len = elements.length; i < len; i++) {
+                item = elements[i];
+                if (callback.call(context || item, item, i, elements) === false) {
+                    break;
+                }
+            }
+        } else if (typeof elements === 'object') {
+            for (key in elements) {
+                if (Object.prototype.hasOwnProperty.call(elements, key) && callback.call(context || elements[key], elements[key], key, elements) === false) {
+                    break;
+                }
+            }
+        }
+    }
+    return elements;
+}
+module.exports = each;
