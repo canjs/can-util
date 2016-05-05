f22f08aed1a88603bc6ee15b71f87705d85fdd26
Justin Meyer <justinbmeyer@gmail.com>
2016-05-05 16:45:08 -0500

    3.0.0-pre.13

---
+++ dist/amd/dom/mutation-observer/mutation-observer.js
@@ -0,0 +1,11 @@
+/*can-util@3.0.0-pre.12#dom/mutation-observer/mutation-observer*/
+define(function (require, exports, module) {
+    var global = require('../../js/global/global')();
+    var setMutationObserver;
+    module.exports = function (setMO) {
+        if (setMO !== undefined) {
+            setMutationObserver = setMO;
+        }
+        return setMutationObserver !== undefined ? setMutationObserver : global.MutationObserver || global.WebKitMutationObserver || global.MozMutationObserver;
+    };
+});
---
+++ dist/cjs/js/is-plain-object/is-plain-object.js
@@ -0,0 +1,22 @@
+/*can-util@3.0.0-pre.12#js/is-plain-object/is-plain-object*/
+var core_hasOwn = Object.prototype.hasOwnProperty;
+function isWindow(obj) {
+    return obj !== null && obj == obj.window;
+}
+function isPlainObject(obj) {
+    if (!obj || typeof obj !== 'object' || obj.nodeType || isWindow(obj)) {
+        return false;
+    }
+    try {
+        if (obj.constructor && !core_hasOwn.call(obj, 'constructor') && !core_hasOwn.call(obj.constructor.prototype, 'isPrototypeOf')) {
+            return false;
+        }
+    } catch (e) {
+        return false;
+    }
+    var key;
+    for (key in obj) {
+    }
+    return key === undefined || core_hasOwn.call(obj, key);
+}
+module.exports = isPlainObject;
