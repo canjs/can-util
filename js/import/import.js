91821b24a163c5b9194f0de608f9a261854c97bf
David Luecke <daff@neyeon.com>
2016-05-10 11:21:15 -0700

    Implementing can.import (#11)

--- js/import/import.js
+++ js/import/import.js
@@ -1,23 +1,20 @@
+var isFunction = require('../is-function/is-function');
+
 module.exports = function(moduleName, parentName) {
-	var deferred = new can.Deferred();
-
-	if(typeof window.System === "object" && can.isFunction(window.System["import"])) {
-		window.System["import"](moduleName, {
-			name: parentName
-		}).then(can.proxy(deferred.resolve, deferred),
-			can.proxy(deferred.reject, deferred));
-	} else if(window.define && window.define.amd){
-
-		window.require([moduleName], function(value){
-			deferred.resolve(value);
-		});
-
-	} else if(window.require){
-		deferred.resolve(window.require(moduleName));
-	} else {
-		// ideally this will use can.getObject
-		deferred.resolve();
-	}
-
-	return deferred.promise();
+	return new Promise(function(resolve, reject) {
+		if(typeof window.System === "object" && isFunction(window.System["import"])) {
+			window.System["import"](moduleName, {
+				name: parentName
+			}).then(resolve, reject);
+		} else if(window.define && window.define.amd){
+			window.require([moduleName], function(value){
+				resolve(value);
+			});
+		} else if(window.require){
+			resolve(window.require(moduleName));
+		} else {
+			// ideally this will use can.getObject
+			resolve();
+		}
+	});
 };
