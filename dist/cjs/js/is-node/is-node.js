148f6147c5e0593133bd36eeea51f733a1e8ad04
David Luecke <daff@neyeon.com>
2016-05-18 14:29:37 -0700

    3.0.0-pre.21

---
+++ dist/cjs/js/is-node/is-node.js
@@ -0,0 +1,4 @@
+/*can-util@3.0.0-pre.20#js/is-node/is-node*/
+module.exports = function () {
+    return typeof process === 'object' && {}.toString.call(process) === '[object process]';
+};
---
+++ dist/cjs/js/last/last.js
@@ -0,0 +1,4 @@
+/*can-util@3.0.0-pre.20#js/last/last*/
+module.exports = function (arr) {
+    return arr && arr[arr.length - 1];
+};
