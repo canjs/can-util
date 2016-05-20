befb50252391500284858a0dd66a150702b4477c
David Luecke <daff@neyeon.com>
2016-05-20 15:54:29 -0700

    3.0.0-pre.22

---
+++ dist/amd/js/is-web-worker/is-web-worker.js
@@ -0,0 +1,6 @@
+/*can-util@3.0.0-pre.21#js/is-web-worker/is-web-worker*/
+define(function (require, exports, module) {
+    module.exports = function () {
+        return typeof WorkerGlobalScope !== 'undefined' && this instanceof WorkerGlobalScope;
+    };
+});
---
+++ dist/amd/js/js.js
@@ -0,0 +1,29 @@
+/*can-util@3.0.0-pre.21#js/js*/
+define(function (require, exports, module) {
+    module.exports = {
+        assign: require('./assign/assign'),
+        cid: require('./cid/cid'),
+        deepExtend: require('./deep-extend/deep-extend'),
+        dev: require('./dev/dev'),
+        diff: require('./diff/diff'),
+        each: require('./each/each'),
+        global: require('./global/global'),
+        import: require('./import/import'),
+        isArray: require('./is-array/is-array'),
+        isArrayLike: require('./is-array-like/is-array-like'),
+        isBrowserWindow: require('./is-browser-window/is-browser-window'),
+        isEmptyObject: require('./is-empty-object/is-empty-object'),
+        isFunction: require('./is-function/is-function'),
+        isNode: require('./is-node/is-node'),
+        isPlainObject: require('./is-plain-object/is-plain-object'),
+        isPromise: require('./is-promise/is-promise'),
+        isString: require('./is-string/is-string'),
+        isWebWorker: require('./is-web-worker/is-web-worker'),
+        joinURIs: require('./join-uris/join-uris'),
+        last: require('./last/last'),
+        makeArra: require('./make-array/make-array'),
+        setImmediate: require('./set-immediate/set-immediate'),
+        string: require('./string/string'),
+        types: require('./types/types')
+    };
+});
