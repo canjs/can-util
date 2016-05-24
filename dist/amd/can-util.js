9e43c75070770a030aa3029429dc39628a7c314c
Matthew Phillips <matthew@matthewphillips.info>
2016-05-24 08:27:54 -0400

    3.0.0-pre.23

---
+++ dist/amd/can-util.js
@@ -0,0 +1,6 @@
+/*can-util@3.0.0-pre.22#can-util*/
+define(function (require, exports, module) {
+    var extend = require('./js/deep-extend/deep-extend');
+    var namespace = require('./namespace');
+    module.exports = extend(namespace, require('./dom/dom'), require('./js/js'));
+});
