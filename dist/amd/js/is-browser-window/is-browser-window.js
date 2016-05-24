940b4d28aa759b3cf16f7a5c219bc51d2ba02974
Matthew Phillips <matthew@matthewphillips.info>
2016-05-24 13:51:43 -0400

    3.0.0-pre.24

---
+++ dist/amd/js/is-browser-window/is-browser-window.js
@@ -0,0 +1,6 @@
+/*can-util@3.0.0-pre.23#js/is-browser-window/is-browser-window*/
+define(function (require, exports, module) {
+    module.exports = function () {
+        return typeof window !== 'undefined' && typeof document !== 'undefined' && typeof SimpleDOM === 'undefined';
+    };
+});
