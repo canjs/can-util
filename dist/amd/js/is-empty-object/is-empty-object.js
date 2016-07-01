900b92edc57f0cc5f3c9f63d1daa2a8dcffe667c
Bradley Momberger <brad@bitovi.com>
2016-07-01 17:35:27 -0400

    3.0.0-pre.29

---
+++ dist/amd/js/is-empty-object/is-empty-object.js
@@ -0,0 +1,9 @@
+/*can-util@3.0.0-pre.28#js/is-empty-object/is-empty-object*/
+define(function (require, exports, module) {
+    module.exports = function (obj) {
+        for (var prop in obj) {
+            return false;
+        }
+        return true;
+    };
+});
---
+++ dist/amd/js/is-node/is-node.js
@@ -0,0 +1,10 @@
+/*can-util@3.0.0-pre.28#js/is-node/is-node*/
+define(function (require, exports, module) {
+    (function (global) {
+        module.exports = function () {
+            return typeof process === 'object' && {}.toString.call(process) === '[object process]';
+        };
+    }(function () {
+        return this;
+    }()));
+});
