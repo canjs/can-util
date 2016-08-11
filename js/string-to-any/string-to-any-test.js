e0d2d5ed84f163c5096aebea5a743b44e5e4b873
Matthew Phillips <matthew@matthewphillips.info>
2016-08-11 15:58:07 -0400

    Adds the stringToAny utility function.
    This implements the stringToAny utility function that converts strings
    back to their primitive types.
    
    Closes #37

---
+++ js/string-to-any/string-to-any-test.js
@@ -0,0 +1,23 @@
+var QUnit = require("../../test/qunit");
+var stringToAny = require("./string-to-any");
+var each = require("../each/each");
+
+QUnit.module("can-util/js/string-to-any");
+
+QUnit.test("works with primitive types", function(){
+	var fixture = {
+		"foo": "foo",
+		"33": 33,
+		"true": true,
+		"false": false,
+		"undefined": undefined,
+		"null": null,
+		"Infinity": Infinity
+	};
+
+	each(fixture, function(value, key){
+		QUnit.ok(stringToAny(key) === value, "Correctly converted type: " + key);
+	});
+
+	QUnit.ok(isNaN(stringToAny("NaN")), "Correclty converted type: NaN");
+});
