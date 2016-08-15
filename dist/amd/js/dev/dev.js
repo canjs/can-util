574d89e851dba79b316c371bb6102c6080c2bd8f
Matthew Phillips <matthew@matthewphillips.info>
2016-08-15 09:42:39 -0400

    3.0.0-pre.34

---
+++ dist/amd/js/dev/dev.js
@@ -0,0 +1,3 @@
+/*can-util@3.0.0-pre.33#js/dev/dev*/
+define(function (require, exports, module) {
+});
---
+++ dist/amd/js/types/types.js
@@ -0,0 +1,39 @@
+/*can-util@3.0.0-pre.33#js/types/types*/
+define(function (require, exports, module) {
+    var isPromise = require('../is-promise/is-promise');
+    var types = {
+        isMapLike: function () {
+            return false;
+        },
+        isListLike: function () {
+            return false;
+        },
+        isPromise: function (obj) {
+            return isPromise(obj);
+        },
+        isConstructor: function (func) {
+            if (typeof func !== 'function') {
+                return false;
+            }
+            for (var prop in func.prototype) {
+                return true;
+            }
+            return false;
+        },
+        isCallableForValue: function (obj) {
+            return typeof obj === 'function' && !types.isConstructor(obj);
+        },
+        isCompute: function (obj) {
+            return obj && obj.isComputed;
+        },
+        DefaultMap: null,
+        DefaultList: null,
+        wrapElement: function (element) {
+            return element;
+        },
+        unwrapElement: function (element) {
+            return element;
+        }
+    };
+    module.exports = types;
+});
