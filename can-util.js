8c007d91c149b241b50d992d0f2bbe5db8dd68b9
Hitesh Ubharani <hubharani@apple.com>
2016-09-05 22:46:34 -0700

    require-fix

--- can-util.js
+++ can-util.js
@@ -1,4 +1,4 @@
-var deepAssign = require('./js/deep-assign/');
+var deepAssign = require('./js/deep-assign/deep-assign');
 var namespace = require('./namespace');
 
-module.exports = deepAssign(namespace, require('./dom/'), require('./js/'));
+module.exports = deepAssign(namespace, require('./dom/dom'), require('./js/js'));
