f91b22824486a8d6f58136a63c18060ce8c9dfa2
Matthew Phillips <matthew@matthewphillips.info>
2016-09-14 10:08:56 -0400

    3.0.0-pre.40

---
+++ dist/amd/dom/contains/contains.js
@@ -0,0 +1,6 @@
+/*can-util@3.0.0-pre.39#dom/contains/contains*/
+define(function (require, exports, module) {
+    module.exports = function (child) {
+        return this.contains(child);
+    };
+});
---
+++ dist/amd/js/types/types.js
@@ -0,0 +1,40 @@
+/*can-util@3.0.0-pre.39#js/types/types*/
+define(function (require, exports, module) {
+    var isPromise = require('../is-promise/is-promise');
+    var types = {
+        isMapLike: function () {
+            return false;
+        },
+        isListLike: function () {
+            return false;
+        },
+        isPromise: function (obj) {
+            return isPromise(obj);
+        },
+        isConstructor: function (func) {
+            if (typeof func !== 'function') {
+                return false;
+            }
+            for (var prop in func.prototype) {
+                return true;
+            }
+            return false;
+        },
+        isCallableForValue: function (obj) {
+            return typeof obj === 'function' && !types.isConstructor(obj);
+        },
+        isCompute: function (obj) {
+            return obj && obj.isComputed;
+        },
+        iterator: typeof Symbol === 'function' && Symbol.iterator || '@@iterator',
+        DefaultMap: null,
+        DefaultList: null,
+        wrapElement: function (element) {
+            return element;
+        },
+        unwrapElement: function (element) {
+            return element;
+        }
+    };
+    module.exports = types;
+});
