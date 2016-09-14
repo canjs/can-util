9f0247d6756737750e950b97910e9ca9b448ac80
Matthew Phillips <matthew@matthewphillips.info>
2016-09-14 08:48:49 -0400

    3.0.0-pre.38

---
+++ dist/amd/dom/frag/frag.js
@@ -0,0 +1,40 @@
+/*can-util@3.0.0-pre.37#dom/frag/frag*/
+define(function (require, exports, module) {
+    var getDocument = require('../document/document');
+    var fragment = require('../fragment/fragment');
+    var each = require('../../js/each/each');
+    var childNodes = require('../child-nodes/child-nodes');
+    var makeFrag = function (item, doc) {
+        var document = doc || getDocument();
+        var frag;
+        if (!item || typeof item === 'string') {
+            frag = fragment(item == null ? '' : '' + item, document);
+            if (!frag.childNodes.length) {
+                frag.appendChild(document.createTextNode(''));
+            }
+            return frag;
+        } else if (item.nodeType === 11) {
+            return item;
+        } else if (typeof item.nodeType === 'number') {
+            frag = document.createDocumentFragment();
+            frag.appendChild(item);
+            return frag;
+        } else if (typeof item.length === 'number') {
+            frag = document.createDocumentFragment();
+            each(item, function (item) {
+                frag.appendChild(makeFrag(item));
+            });
+            if (!childNodes(frag).length) {
+                frag.appendChild(document.createTextNode(''));
+            }
+            return frag;
+        } else {
+            frag = fragment('' + item, document);
+            if (!childNodes(frag).length) {
+                frag.appendChild(document.createTextNode(''));
+            }
+            return frag;
+        }
+    };
+    module.exports = makeFrag;
+});
