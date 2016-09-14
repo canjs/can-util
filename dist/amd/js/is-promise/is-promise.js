07260938cd63ed23559734d5ac8fcc1d94209b3d
Matthew Phillips <matthew@matthewphillips.info>
2016-09-14 09:29:25 -0400

    3.0.0-pre.39

---
+++ dist/amd/js/is-promise/is-promise.js
@@ -0,0 +1,6 @@
+/*can-util@3.0.0-pre.38#js/is-promise/is-promise*/
+define(function (require, exports, module) {
+    module.exports = function (obj) {
+        return !!obj && (typeof obj === 'object' || typeof obj === 'function') && typeof obj.then === 'function';
+    };
+});
