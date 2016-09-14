3daa90b8352a6236c7be265945b6fb49024a237d
Matthew Phillips <matthew@matthewphillips.info>
2016-09-14 13:53:53 -0400

    3.0.0-pre.42

---
+++ dist/amd/dom/data/data.js
@@ -0,0 +1,35 @@
+/*can-util@3.0.0-pre.41#dom/data/data*/
+define(function (require, exports, module) {
+    var isEmptyObject = require('../../js/is-empty-object/is-empty-object');
+    var data = {};
+    var expando = 'can' + new Date();
+    var uuid = 0;
+    var setData = function (name, value) {
+        var id = this[expando] || (this[expando] = ++uuid), store = data[id] || (data[id] = {});
+        if (name !== undefined) {
+            store[name] = value;
+        }
+        return store;
+    };
+    module.exports = {
+        getCid: function () {
+            return this[expando];
+        },
+        cid: function () {
+            return this[expando] || (this[expando] = ++uuid);
+        },
+        expando: expando,
+        clean: function (prop) {
+            var id = this[expando];
+            delete data[id][prop];
+            if (isEmptyObject(data[id])) {
+                delete data[id];
+            }
+        },
+        get: function (key) {
+            var id = this[expando], store = id && data[id];
+            return key === undefined ? store || setData(this) : store && store[key];
+        },
+        set: setData
+    };
+});
---
+++ dist/amd/js/is-browser-window/is-browser-window.js
@@ -0,0 +1,10 @@
+/*can-util@3.0.0-pre.41#js/is-browser-window/is-browser-window*/
+define(function (require, exports, module) {
+    (function (global) {
+        module.exports = function () {
+            return typeof window !== 'undefined' && typeof document !== 'undefined' && typeof SimpleDOM === 'undefined';
+        };
+    }(function () {
+        return this;
+    }()));
+});
