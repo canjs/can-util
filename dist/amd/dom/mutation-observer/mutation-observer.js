edaa394b05ff8af53337f88988d3950835bd605e
Matthew Phillips <matthew@matthewphillips.info>
2016-09-16 08:40:00 -0400

    3.0.0-pre.47

---
+++ dist/amd/dom/mutation-observer/mutation-observer.js
@@ -0,0 +1,15 @@
+/*can-util@3.0.0-pre.46#dom/mutation-observer/mutation-observer*/
+define(function (require, exports, module) {
+    (function (global) {
+        var global = require('../../js/global/global')();
+        var setMutationObserver;
+        module.exports = function (setMO) {
+            if (setMO !== undefined) {
+                setMutationObserver = setMO;
+            }
+            return setMutationObserver !== undefined ? setMutationObserver : global.MutationObserver || global.WebKitMutationObserver || global.MozMutationObserver;
+        };
+    }(function () {
+        return this;
+    }()));
+});
