383dfb3ecc1ea713e46423f64edc83f18534e712
Matthew Phillips <matthew@matthewphillips.info>
2016-09-16 11:19:31 -0400

    3.0.0-pre.48

---
+++ dist/amd/js/is-iterable/is-iterable.js
@@ -0,0 +1,7 @@
+/*can-util@3.0.0-pre.47#js/is-iterable/is-iterable*/
+define(function (require, exports, module) {
+    var types = require('../types/types');
+    module.exports = function (obj) {
+        return obj && !!obj[types.iterator];
+    };
+});
