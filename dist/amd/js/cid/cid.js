6938539a23399f43eecfe077081fe4d6611970a5
Matthew Phillips <matthew@matthewphillips.info>
2016-09-16 14:49:14 -0400

    3.0.0-pre.50

---
+++ dist/amd/js/cid/cid.js
@@ -0,0 +1,11 @@
+/*can-util@3.0.0-pre.49#js/cid/cid*/
+define(function (require, exports, module) {
+    var cid = 0;
+    module.exports = function (object, name) {
+        if (!object._cid) {
+            cid++;
+            object._cid = (name || '') + cid;
+        }
+        return object._cid;
+    };
+});
