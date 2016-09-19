88eca38c854b641cc2269ee4ad7943aa3710d42c
Matthew Phillips <matthew@matthewphillips.info>
2016-09-19 09:21:43 -0400

    3.0.0-pre.52

---
+++ dist/amd/js/types/types.js
@@ -0,0 +1,43 @@
+/*can-util@3.0.0-pre.51#js/types/types*/
+define(function (require, exports, module) {
+    var isPromise = require('../is-promise/is-promise');
+    var types = {
+        afterEvents: function (fn) {
+            fn();
+        },
+        isMapLike: function () {
+            return false;
+        },
+        isListLike: function () {
+            return false;
+        },
+        isPromise: function (obj) {
+            return isPromise(obj);
+        },
+        isConstructor: function (func) {
+            if (typeof func !== 'function') {
+                return false;
+            }
+            for (var prop in func.prototype) {
+                return true;
+            }
+            return false;
+        },
+        isCallableForValue: function (obj) {
+            return typeof obj === 'function' && !types.isConstructor(obj);
+        },
+        isCompute: function (obj) {
+            return obj && obj.isComputed;
+        },
+        iterator: typeof Symbol === 'function' && Symbol.iterator || '@@iterator',
+        DefaultMap: null,
+        DefaultList: null,
+        wrapElement: function (element) {
+            return element;
+        },
+        unwrapElement: function (element) {
+            return element;
+        }
+    };
+    module.exports = types;
+});
