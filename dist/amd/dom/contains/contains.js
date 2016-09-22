78f44669f82d62995f92006fa998dc1c0f4fb8e4
David Luecke <daff@neyeon.com>
2016-09-22 10:46:26 -0700

    3.0.0-pre.55

---
+++ dist/amd/dom/contains/contains.js
@@ -0,0 +1,6 @@
+/*can-util@3.0.0-pre.54#dom/contains/contains*/
+define(function (require, exports, module) {
+    module.exports = function (child) {
+        return this.contains(child);
+    };
+});
