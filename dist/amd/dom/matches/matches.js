1bff1e74b0ec46b529a3cfbc03bc6683fcff8175
David Luecke <daff@neyeon.com>
2016-09-23 15:40:44 -0700

    3.0.0-pre.57

---
+++ dist/amd/dom/matches/matches.js
@@ -0,0 +1,10 @@
+/*can-util@3.0.0-pre.56#dom/matches/matches*/
+define(function (require, exports, module) {
+    var matchesMethod = function (element) {
+        return element.matches || element.webkitMatchesSelector || element.webkitMatchesSelector || element.mozMatchesSelector || element.msMatchesSelector || element.oMatchesSelector;
+    };
+    module.exports = function () {
+        var method = matchesMethod(this);
+        return method ? method.apply(this, arguments) : false;
+    };
+});
