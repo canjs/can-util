7ba5f18b5c6015477404d3ae2b37ebfb019e5621
Justin Meyer <justinbmeyer@gmail.com>
2016-09-28 12:56:18 -0500

    3.0.0-pre.60

---
+++ dist/amd/dom/dispatch/dispatch.js
@@ -0,0 +1,16 @@
+/*can-util@3.0.0-pre.59#dom/dispatch/dispatch*/
+define(function (require, exports, module) {
+    var assign = require('../../js/assign/assign');
+    var _document = require('../document/document');
+    module.exports = function (event, args, bubbles) {
+        var doc = _document();
+        var ev = doc.createEvent('HTMLEvents');
+        var isString = typeof event === 'string';
+        ev.initEvent(isString ? event : event.type, bubbles === undefined ? true : bubbles, false);
+        if (!isString) {
+            assign(ev, event);
+        }
+        ev.args = args;
+        return this.dispatchEvent(ev);
+    };
+});
