29dc885d0d0520809d28ae371ed9a55a22f2ec03
Kevin Phillips <kphillips86@gmail.com>
2016-09-30 08:49:29 -0500

    3.0.0-pre.62

---
+++ dist/cjs/dom/dom.js
@@ -0,0 +1,17 @@
+/*can-util@3.0.0-pre.16#dom/dom*/
+module.exports = {
+    attr: require('./attr/attr.js'),
+    childNodes: require('./child-nodes/child-nodes.js'),
+    className: require('./class-name/class-name.js'),
+    contains: require('./contains/contains.js'),
+    data: require('./data/data.js'),
+    dispatch: require('./dispatch/dispatch.js'),
+    document: require('./document/document.js'),
+    events: require('./events/events.js'),
+    frag: require('./frag/frag.js'),
+    fragment: require('./fragment/fragment.js'),
+    isOfGlobalDocument: require('./is-of-global-document/is-of-global-document.js'),
+    matches: require('./matches/matches.js'),
+    mutate: require('./mutate/mutate.js'),
+    mutationObserver: require('./mutation-observer/mutation-observer.js')
+};
