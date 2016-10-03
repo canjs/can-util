381f8a7e0462321eaa58c6d2f9f8ba9e799c83c3
Kevin Phillips <kphillips86@gmail.com>
2016-10-03 11:29:03 -0500

    3.0.0-pre.63

---
+++ dist/amd/js/is-array-like/is-array-like.js
@@ -0,0 +1,12 @@
+/*can-util@3.0.0-pre.62#js/is-array-like/is-array-like*/
+define(function (require, exports, module) {
+    function isArrayLike(obj) {
+        var type = typeof obj;
+        if (type === 'string') {
+            return true;
+        }
+        var length = obj && type !== 'boolean' && typeof obj !== 'number' && 'length' in obj && obj.length;
+        return typeof arr !== 'function' && (length === 0 || typeof length === 'number' && length > 0 && length - 1 in obj);
+    }
+    module.exports = isArrayLike;
+});
---
+++ dist/amd/namespace.js
@@ -0,0 +1,4 @@
+/*can-util@3.0.0-pre.62#namespace*/
+define(function (require, exports, module) {
+    module.exports = {};
+});
