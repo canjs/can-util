e6de116328422c215854bc48e6b8b8feba76fef2
Kevin Phillips <kphillips86@gmail.com>
2016-10-06 14:03:24 -0500

    3.0.0-pre.66

---
+++ dist/amd/dom/contains/contains.js
@@ -0,0 +1,6 @@
+/*can-util@3.0.0-pre.65#dom/contains/contains*/
+define(function (require, exports, module) {
+    module.exports = function (child) {
+        return this.contains(child);
+    };
+});
---
+++ dist/amd/js/is-promise/is-promise.js
@@ -0,0 +1,6 @@
+/*can-util@3.0.0-pre.65#js/is-promise/is-promise*/
+define(function (require, exports, module) {
+    module.exports = function (obj) {
+        return obj instanceof Promise || Object.prototype.toString.call(obj) === '[object Promise]';
+    };
+});
