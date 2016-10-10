d21183e97cfcc0ddcaad22118d9aab8b205ee433
Kevin Phillips <kphillips86@gmail.com>
2016-10-10 14:26:33 -0500

    3.0.0-pre.68

---
+++ dist/amd/can-util.js
@@ -0,0 +1,6 @@
+/*can-util@3.0.0-pre.67#can-util*/
+define(function (require, exports, module) {
+    var deepAssign = require('./js/deep-assign/deep-assign');
+    var namespace = require('./namespace');
+    module.exports = deepAssign(namespace, require('./dom/dom'), require('./js/js'));
+});
