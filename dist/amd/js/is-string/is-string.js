432a0b1151c58b6de17de5cd60b9e61d956ce5d9
John Gardner <john@ardea.io>
2016-10-12 15:36:35 -0500

    3.0.0-pre.69

---
+++ dist/amd/js/is-string/is-string.js
@@ -0,0 +1,6 @@
+/*can-util@3.0.0-pre.68#js/is-string/is-string*/
+define(function (require, exports, module) {
+    module.exports = function isString(obj) {
+        return typeof obj === 'string';
+    };
+});
---
+++ dist/amd/namespace.js
@@ -0,0 +1,4 @@
+/*can-util@3.0.0-pre.68#namespace*/
+define(function (require, exports, module) {
+    module.exports = {};
+});
