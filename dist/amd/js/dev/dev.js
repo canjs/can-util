77d726c66b7ace38d8b106e2ca516c3d7863172f
John Gardner <john@ardea.io>
2016-10-13 21:14:42 -0500

    3.0.0-pre.70

---
+++ dist/amd/js/dev/dev.js
@@ -0,0 +1,3 @@
+/*can-util@3.0.0-pre.69#js/dev/dev*/
+define(function (require, exports, module) {
+});
---
+++ dist/amd/js/last/last.js
@@ -0,0 +1,6 @@
+/*can-util@3.0.0-pre.69#js/last/last*/
+define(function (require, exports, module) {
+    module.exports = function (arr) {
+        return arr && arr[arr.length - 1];
+    };
+});
