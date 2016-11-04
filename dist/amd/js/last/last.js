f9317f4998e468983941be8c88a5c7b7e96e5605
Justin Meyer <justinbmeyer@gmail.com>
2016-11-04 13:09:05 -0500

    3.0.7

---
+++ dist/amd/js/last/last.js
@@ -0,0 +1,6 @@
+/*can-util@3.0.6#js/last/last*/
+define(function (require, exports, module) {
+    module.exports = function (arr) {
+        return arr && arr[arr.length - 1];
+    };
+});
---
+++ dist/amd/namespace.js
@@ -0,0 +1,4 @@
+/*can-util@3.0.6#namespace*/
+define(function (require, exports, module) {
+    module.exports = {};
+});
