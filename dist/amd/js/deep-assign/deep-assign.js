a331bff11ae08ab5cd48b7249b00ec123bea311e
Justin Meyer <justinbmeyer@gmail.com>
2016-11-04 11:52:32 -0500

    3.0.5

---
+++ dist/amd/js/deep-assign/deep-assign.js
@@ -0,0 +1,40 @@
+/*can-util@3.0.4#js/deep-assign/deep-assign*/
+define(function (require, exports, module) {
+    var isArray = require('../is-array/is-array');
+    var isFunction = require('../is-function/is-function');
+    var isPlainObject = require('../is-plain-object/is-plain-object');
+    function deepAssign() {
+        var options, name, src, copy, copyIsArray, clone, target = arguments[0] || {}, i = 1, length = arguments.length;
+        if (typeof target !== 'object' && !isFunction(target)) {
+            target = {};
+        }
+        if (length === i) {
+            target = this;
+            --i;
+        }
+        for (; i < length; i++) {
+            if ((options = arguments[i]) != null) {
+                for (name in options) {
+                    src = target[name];
+                    copy = options[name];
+                    if (target === copy) {
+                        continue;
+                    }
+                    if (copy && (isPlainObject(copy) || (copyIsArray = isArray(copy)))) {
+                        if (copyIsArray) {
+                            copyIsArray = false;
+                            clone = src && isArray(src) ? src : [];
+                        } else {
+                            clone = src && isPlainObject(src) ? src : {};
+                        }
+                        target[name] = deepAssign(clone, copy);
+                    } else if (copy !== undefined) {
+                        target[name] = copy;
+                    }
+                }
+            }
+        }
+        return target;
+    }
+    module.exports = deepAssign;
+});
---
+++ dist/amd/js/is-array/is-array.js
@@ -0,0 +1,6 @@
+/*can-util@3.0.4#js/is-array/is-array*/
+define(function (require, exports, module) {
+    module.exports = function (arr) {
+        return Array.isArray(arr);
+    };
+});
