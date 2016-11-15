9b9011d818eded7dee609899714206bcc01c4c4a
Kevin Phillips <kphillips86@gmail.com>
2016-11-15 16:16:31 -0600

    3.0.11

---
+++ dist/amd/js/is-string/is-string.js
@@ -0,0 +1,6 @@
+/*can-util@3.0.10#js/is-string/is-string*/
+define(function (require, exports, module) {
+    module.exports = function isString(obj) {
+        return typeof obj === 'string';
+    };
+});
