3e39a12c9d837a67e7270526a81a4862d3bb4dbf
Matthew Phillips <matthew@matthewphillips.info>
2016-12-19 11:24:56 -0500

    3.2.0

---
+++ dist/amd/js/each/each.js
@@ -0,0 +1,35 @@
+/*can-util@3.1.1#js/each/each*/
+define(function (require, exports, module) {
+    var isArrayLike = require('../is-array-like/is-array-like');
+    var has = Object.prototype.hasOwnProperty;
+    var isIterable = require('../is-iterable/is-iterable');
+    var types = require('can-types');
+    function each(elements, callback, context) {
+        var i = 0, key, len, item;
+        if (elements) {
+            if (isArrayLike(elements)) {
+                for (len = elements.length; i < len; i++) {
+                    item = elements[i];
+                    if (callback.call(context || item, item, i, elements) === false) {
+                        break;
+                    }
+                }
+            } else if (isIterable(elements)) {
+                var iter = elements[types.iterator]();
+                var res, value;
+                while (!(res = iter.next()).done) {
+                    value = res.value;
+                    callback.call(context || elements, Array.isArray(value) ? value[1] : value, value[0]);
+                }
+            } else if (typeof elements === 'object') {
+                for (key in elements) {
+                    if (has.call(elements, key) && callback.call(context || elements[key], elements[key], key, elements) === false) {
+                        break;
+                    }
+                }
+            }
+        }
+        return elements;
+    }
+    module.exports = each;
+});
