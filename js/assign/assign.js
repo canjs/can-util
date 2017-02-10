258c817141c95bc55b3b7a9edc82345ac970f72d
cherif BOUCHELAGHEM <cherif.bouchelaghem@gmail.com>
2017-02-10 20:25:56 +0100

    change assign behavior to mimic Object.assign behavior

--- js/assign/assign.js
+++ js/assign/assign.js
@@ -1,7 +1,7 @@
 /**
  * @module {function} can-util/js/assign/assign assign
  * @parent can-util/js
- * @signature `assign(target, source)`
+ * @signature `assign(target, args)`
  *
  * A simplified version of [Object.assign](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign), which only accepts a single source argument.
  *
@@ -18,15 +18,25 @@
  * ```
  *
  * @param {Object} target The destination object. This object's properties will be mutated based on the object provided as `source`.
- * @param {Object} source The source object whose own properties will be applied to `target`.
+ * @param {Object} args The source objects whose own properties will be applied to `target`.
  *
  * @return {Object} Returns the `target` argument.
  */
-module.exports = function (d, s) {
-	for (var prop in s) {
-		if (Object.prototype.hasOwnProperty.call(s, prop)) {
-			d[prop] = s[prop];
+
+module.exports = function (target, args) {
+	if (target === null) {
+  	throw new TypeError('The target object cannot be undefined or null');
+  }
+	for (var i = 1; i < arguments.length; i++) {
+		var supplier = arguments[i];
+
+		if (supplier !=null) {
+			for (var prop in supplier) {
+				if (Object.prototype.hasOwnProperty.call(supplier, prop)) {
+					target[prop] = supplier[prop];
+				}
+			}
 		}
 	}
-	return d;
+	return target;
 };
