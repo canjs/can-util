ca1189f9fec0cf4ecbd504d4df200554f7d5996a
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-02-28 00:43:12 -0500

    3.3.1

---
+++ dist/amd/dom/contains/contains.js
@@ -0,0 +1,6 @@
+/*can-util@3.3.0#dom/contains/contains*/
+define(function (require, exports, module) {
+    module.exports = function (child) {
+        return this.contains(child);
+    };
+});
---
+++ dist/amd/js/param/param.js
@@ -0,0 +1,27 @@
+/*can-util@3.3.0#js/param/param*/
+define(function (require, exports, module) {
+    var isArray = require('../is-array/is-array');
+    var namespace = require('can-namespace');
+    function buildParam(prefix, obj, add) {
+        if (isArray(obj)) {
+            for (var i = 0, l = obj.length; i < l; ++i) {
+                add(prefix + '[]', obj[i]);
+            }
+        } else if (obj && typeof obj === 'object') {
+            for (var name in obj) {
+                buildParam(prefix + '[' + name + ']', obj[name], add);
+            }
+        } else {
+            add(prefix, obj);
+        }
+    }
+    module.exports = namespace.param = function param(object) {
+        var pairs = [], add = function (key, value) {
+                pairs.push(encodeURIComponent(key) + '=' + encodeURIComponent(value));
+            };
+        for (var name in object) {
+            buildParam(name, object[name], add);
+        }
+        return pairs.join('&').replace(/%20/g, '+');
+    };
+});
