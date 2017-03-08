600ac01ffd3696452168a032ee8ba765895b29f4
Kevin Phillips <kevin@bitovi.com>
2017-03-08 17:19:23 -0600

    3.3.2

---
+++ dist/amd/dom/contains/contains.js
@@ -0,0 +1,6 @@
+/*can-util@3.3.1#dom/contains/contains*/
+define(function (require, exports, module) {
+    module.exports = function (child) {
+        return this.contains(child);
+    };
+});
---
+++ dist/amd/dom/is-of-global-document/is-of-global-document.js
@@ -0,0 +1,7 @@
+/*can-util@3.3.1#dom/is-of-global-document/is-of-global-document*/
+define(function (require, exports, module) {
+    var getDocument = require('../document/document');
+    module.exports = function (el) {
+        return (el.ownerDocument || el) === getDocument();
+    };
+});
