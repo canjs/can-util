e06905b8ea49d7503c8768877bd5f219ae34481f
Chasen Le Hara <chasen@chasenlehara.com>
2017-03-30 09:30:41 -0700

    Add a deprecation notice for js/param/param
    Also replace our internal use of it with `can-param`.

--- js/param/param.js
+++ js/param/param.js
@@ -1,6 +1,13 @@
+var canDev = require("../dev/dev");
+
 /**
  * @module can-util/js/param/param param
  * @parent can-util/js
  * @description Deprecated. Use [can-param] instead.
  */
+
+ //!steal-remove-start
+ canDev.warn('js/param/param is deprecated; please use can-param instead: https://github.com/canjs/can-param');
+ //!steal-remove-end
+
 module.exports = require('can-param');
