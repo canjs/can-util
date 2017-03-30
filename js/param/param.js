2240ce2d4f554b049c4b581b91b6084fa0ff3d1c
Chasen Le Hara <chasen@chasenlehara.com>
2017-03-29 22:44:31 -0700

    Use the new can-param module
    - Add `can-param` as a dependency
    - Replace the `can-util/js/param/param` docs with a deprecation notice
    - Replace the `can-util/js/param/param` implementation with just a reference to the new module
    - Remove the `can-util/js/param/param`tests
    
    Related to #195

--- js/param/param.js
+++ js/param/param.js
@@ -1,28 +1,6 @@
-var isArray = require('../is-array/is-array');
-var namespace = require("can-namespace");
-
-function buildParam(prefix, obj, add) {
-	if (isArray(obj)) {
-		for (var i = 0, l = obj.length; i < l; ++i) {
-			add(prefix + '[]', obj[i]);
-		}
-	} else if ( obj && typeof obj === "object" ) {
-		for (var name in obj) {
-			buildParam(prefix + '[' + name + ']', obj[name], add);
-		}
-	} else {
-		add(prefix, obj);
-	}
-}
-
-module.exports = namespace.param = function param(object) {
-	var pairs = [],
-		add = function (key, value) {
-			pairs.push(encodeURIComponent(key) + '=' + encodeURIComponent(value));
-		};
-	for (var name in object) {
-		buildParam(name, object[name], add);
-	}
-	return pairs.join('&')
-		.replace(/%20/g, '+');
-};
+/**
+ * @module can-util/js/param/param param
+ * @parent can-util/js
+ * @description Deprecated. Use [can-param] instead.
+ */
+module.exports = require('can-param');
