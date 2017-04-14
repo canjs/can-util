acc32fcff154b342b71c06ee101140656c9e61c4
Justin Meyer <justinbmeyer@gmail.com>
2017-04-14 12:30:12 -0500

    3.3.6

---
+++ dist/amd/dom/contains/contains.js
@@ -0,0 +1,6 @@
+/*can-util@3.3.5#dom/contains/contains*/
+define(function (require, exports, module) {
+    module.exports = function (child) {
+        return this.contains(child);
+    };
+});
---
+++ dist/amd/dom/document/document.js
@@ -0,0 +1,15 @@
+/*can-util@3.3.5#dom/document/document*/
+define(function (require, exports, module) {
+    (function (global) {
+        var global = require('../../js/global/global');
+        var setDocument;
+        module.exports = function (setDoc) {
+            if (setDoc) {
+                setDocument = setDoc;
+            }
+            return setDocument || global().document;
+        };
+    }(function () {
+        return this;
+    }()));
+});
