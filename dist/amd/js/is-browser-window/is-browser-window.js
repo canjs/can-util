0fd16d2d02cadf79d85150676f72f04c870a4241
Chris Andrejewski <christopher.andrejewski@gmail.com>
2017-04-25 12:25:10 -0400

    3.4.1

---
+++ dist/amd/js/is-browser-window/is-browser-window.js
@@ -0,0 +1,11 @@
+/*can-util@3.4.0#js/is-browser-window/is-browser-window*/
+define(function (require, exports, module) {
+    (function (global) {
+        'use strict';
+        module.exports = function () {
+            return typeof window !== 'undefined' && typeof document !== 'undefined' && typeof SimpleDOM === 'undefined';
+        };
+    }(function () {
+        return this;
+    }()));
+});
