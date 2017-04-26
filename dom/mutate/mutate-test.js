716bb7bccd97ea1b559f1b24f0c725ce866803b0
Matthew Phillips <matthew@matthewphillips.info>
2017-04-26 17:58:24 -0400

    Check if an element is contained within the documentElement
    When document.contains doesn't exist, use the documentElement rather than the body. This enables removing the body and having the "removed" event fire on every element. Closes #251

--- dom/mutate/mutate-test.js
+++ dom/mutate/mutate-test.js
@@ -3,6 +3,7 @@
 var mutate = require('./mutate');
 var MUTATION_OBSERVER = require("../mutation-observer/mutation-observer");
 var DOCUMENT = require("../document/document");
+var makeDocument = require("can-vdom/make-document/make-document");
 
 QUnit = require('steal-qunit');
 
@@ -32,7 +33,46 @@ test("inserting empty frag", function () {
 		enableMO();
 		start();
 	},10);
+});
 
+test("removing the body causes removed events", function () {
+	var enableMO = disableMO();
+	var oldDoc = DOCUMENT();
+
+	var doc = makeDocument();
+	DOCUMENT(doc);
+
+	var div = doc.createElement("div");
+	mutate.appendChild.call(doc.body, div);
+
+	div.addEventListener("removed", function(){
+		ok(true, "called");
+	});
+
+	mutate.removeChild.call(doc.documentElement, doc.body);
+
+	stop();
+	setTimeout(function(){
+		enableMO();
+		DOCUMENT(oldDoc);
+		start();
+	},10);
+
+	/*
+	var frag = document.createDocumentFragment();
+	mutate.appendChild.call( document.getElementById("qunit-fixture"), frag );
+
+	var div = document.createElement("div");
+	div.addEventListener("inserted", function(){
+		ok(true, "called");
+	});
+	mutate.appendChild.call( document.getElementById("qunit-fixture"), div );
+	stop();
+	setTimeout(function(){
+		enableMO();
+		start();
+	},10);
+	*/
 });
 
 if(window.eventsBubble) {
