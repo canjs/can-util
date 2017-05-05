ce2c4c20fd8f6c6e300790cda169d07a50cf50d4
Bradley Momberger <brad@bitovi.com>
2017-05-05 12:49:22 -0600

    3.6.2

---
+++ dist/amd/js/dev/dev.js
@@ -0,0 +1,16 @@
+/*can-util@3.6.1#js/dev/dev*/
+define(function (require, exports, module) {
+    'use strict';
+    var canLog = require('../log/log');
+    module.exports = {
+        warnTimeout: 5000,
+        logLevel: 0,
+        warn: function () {
+        },
+        log: function () {
+        },
+        error: function () {
+        },
+        _logger: canLog._logger
+    };
+});
---
+++ dist/amd/js/is-container/is-container.js
@@ -0,0 +1,7 @@
+/*can-util@3.6.1#js/is-container/is-container*/
+define(function (require, exports, module) {
+    'use strict';
+    module.exports = function (current) {
+        return /^f|^o/.test(typeof current);
+    };
+});
