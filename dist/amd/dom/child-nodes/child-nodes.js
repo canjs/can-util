abb1f8e88a3ccdc0c1178143431e3d469a8a7e2c
Alfredo Delgado <alfred@adelgado.org>
2017-05-09 15:00:52 -0400

    3.7.0

---
+++ dist/amd/dom/child-nodes/child-nodes.js
@@ -0,0 +1,19 @@
+/*can-util@3.6.2#dom/child-nodes/child-nodes*/
+define(function (require, exports, module) {
+    'use strict';
+    function childNodes(node) {
+        var childNodes = node.childNodes;
+        if ('length' in childNodes) {
+            return childNodes;
+        } else {
+            var cur = node.firstChild;
+            var nodes = [];
+            while (cur) {
+                nodes.push(cur);
+                cur = cur.nextSibling;
+            }
+            return nodes;
+        }
+    }
+    module.exports = childNodes;
+});
---
+++ dist/amd/js/is-iterable/is-iterable.js
@@ -0,0 +1,8 @@
+/*can-util@3.6.2#js/is-iterable/is-iterable*/
+define(function (require, exports, module) {
+    'use strict';
+    var types = require('can-types');
+    module.exports = function (obj) {
+        return obj && !!obj[types.iterator];
+    };
+});
