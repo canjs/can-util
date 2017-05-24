83a78bccf2be00e16071c66fd9acc0477e72e5ae
John Murphy <chiefsmurph@gmail.com>
2017-05-24 01:10:29 -0700

    3.8.0

---
+++ dist/amd/dom/events/events.js
@@ -0,0 +1,70 @@
+/*can-util@3.7.0#dom/events/events*/
+define(function (require, exports, module) {
+    'use strict';
+    var _document = require('../document/document');
+    var isBrowserWindow = require('../../js/is-browser-window/is-browser-window');
+    var isPlainObject = require('../../js/is-plain-object/is-plain-object');
+    var fixSyntheticEventsOnDisabled = false;
+    function isDispatchingOnDisabled(element, ev) {
+        var isInsertedOrRemoved = isPlainObject(ev) ? ev.type === 'inserted' || ev.type === 'removed' : ev === 'inserted' || ev === 'removed';
+        var isDisabled = !!element.disabled;
+        return isInsertedOrRemoved && isDisabled;
+    }
+    module.exports = {
+        addEventListener: function () {
+            this.addEventListener.apply(this, arguments);
+        },
+        removeEventListener: function () {
+            this.removeEventListener.apply(this, arguments);
+        },
+        canAddEventListener: function () {
+            return this.nodeName && (this.nodeType === 1 || this.nodeType === 9) || this === window;
+        },
+        dispatch: function (event, args, bubbles) {
+            var doc = _document();
+            var ret;
+            var dispatchingOnDisabled = fixSyntheticEventsOnDisabled && isDispatchingOnDisabled(this, event);
+            var ev = doc.createEvent('HTMLEvents');
+            var isString = typeof event === 'string';
+            ev.initEvent(isString ? event : event.type, bubbles === undefined ? true : bubbles, false);
+            if (!isString) {
+                for (var prop in event) {
+                    if (ev[prop] === undefined) {
+                        ev[prop] = event[prop];
+                    }
+                }
+            }
+            ev.args = args;
+            if (dispatchingOnDisabled) {
+                this.disabled = false;
+            }
+            ret = this.dispatchEvent(ev);
+            if (dispatchingOnDisabled) {
+                this.disabled = true;
+            }
+            return ret;
+        }
+    };
+    (function () {
+        if (!isBrowserWindow()) {
+            return;
+        }
+        var testEventName = 'fix_synthetic_events_on_disabled_test';
+        var input = document.createElement('input');
+        input.disabled = true;
+        var timer = setTimeout(function () {
+            fixSyntheticEventsOnDisabled = true;
+        }, 50);
+        var onTest = function onTest() {
+            clearTimeout(timer);
+            module.exports.removeEventListener.call(input, testEventName, onTest);
+        };
+        module.exports.addEventListener.call(input, testEventName, onTest);
+        try {
+            module.exports.dispatch.call(input, testEventName, [], false);
+        } catch (e) {
+            onTest();
+            fixSyntheticEventsOnDisabled = true;
+        }
+    }());
+});
