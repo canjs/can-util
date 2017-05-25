bb38abbd45262a7d8b684516f3da27d06020f1eb
Chris Andrejewski <christopher.andrejewski@gmail.com>
2017-05-25 13:50:01 -0400

    3.8.1

---
+++ dist/amd/js/is-empty-object/is-empty-object.js
@@ -0,0 +1,10 @@
+/*can-util@3.8.0#js/is-empty-object/is-empty-object*/
+define(function (require, exports, module) {
+    'use strict';
+    module.exports = function (obj) {
+        for (var prop in obj) {
+            return false;
+        }
+        return true;
+    };
+});
