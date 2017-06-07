489f0885037355220ce1827d0ea1c152b4288a73
Kevin Phillips <kevin@bitovi.com>
2017-06-06 21:54:23 -0500

    3.8.3

---
+++ dist/amd/dom/data/data.js
@@ -0,0 +1,30 @@
+/*can-util@3.8.2#dom/data/data*/
+define(function (require, exports, module) {
+    'use strict';
+    var domDataCore = require('./core');
+    var mutationDocument = require('../mutation-observer/document/document');
+    var deleteNode = function () {
+        return domDataCore.delete.call(this);
+    };
+    var elementSetCount = 0;
+    var cleanupDomData = function (node) {
+        elementSetCount -= deleteNode.call(node) ? 1 : 0;
+        if (elementSetCount === 0) {
+            mutationDocument.offAfterRemovedNodes(cleanupDomData);
+        }
+    };
+    module.exports = {
+        getCid: domDataCore.getCid,
+        cid: domDataCore.cid,
+        expando: domDataCore.expando,
+        clean: domDataCore.clean,
+        get: domDataCore.get,
+        set: function (name, value) {
+            if (elementSetCount === 0) {
+                mutationDocument.onAfterRemovedNodes(cleanupDomData);
+            }
+            elementSetCount += domDataCore.set.call(this, name, value) ? 1 : 0;
+        },
+        delete: deleteNode
+    };
+});
---
+++ dist/amd/dom/dom.js
@@ -0,0 +1,25 @@
+/*can-util@3.8.2#dom/dom*/
+define(function (require, exports, module) {
+    (function (global) {
+        'use strict';
+        module.exports = {
+            ajax: require('./ajax/ajax'),
+            attr: require('./attr/attr'),
+            childNodes: require('./child-nodes/child-nodes'),
+            className: require('./class-name/class-name'),
+            contains: require('./contains/contains'),
+            data: require('./data/data'),
+            dispatch: require('./dispatch/dispatch'),
+            document: require('./document/document'),
+            events: require('./events/events'),
+            frag: require('./frag/frag'),
+            fragment: require('./fragment/fragment'),
+            isOfGlobalDocument: require('./is-of-global-document/is-of-global-document'),
+            matches: require('./matches/matches'),
+            mutate: require('./mutate/mutate'),
+            mutationObserver: require('./mutation-observer/mutation-observer')
+        };
+    }(function () {
+        return this;
+    }()));
+});
