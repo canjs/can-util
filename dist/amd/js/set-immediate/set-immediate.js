a6a71c07718ff14802470e0f3add9440aeff2564
Kevin Phillips <kevin@bitovi.com>
2017-06-12 20:28:24 -0500

    3.9.0-pre.1

---
+++ dist/amd/js/set-immediate/set-immediate.js
@@ -0,0 +1,12 @@
+/*can-util@3.9.0-pre.0#js/set-immediate/set-immediate*/
+define(function (require, exports, module) {
+    (function (global) {
+        'use strict';
+        var global = require('../global/global')();
+        module.exports = global.setImmediate || function (cb) {
+            return setTimeout(cb, 0);
+        };
+    }(function () {
+        return this;
+    }()));
+});
