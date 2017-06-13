e77a8d8eb6a51512b40b2b416f1ff566a50a2008
Kevin Phillips <kevin@bitovi.com>
2017-06-13 14:06:35 -0500

    3.9.0-pre.2

---
+++ dist/amd/js/is-array-like/is-array-like.js
@@ -0,0 +1,15 @@
+/*can-util@3.8.4#js/is-array-like/is-array-like*/
+define(function (require, exports, module) {
+    'use strict';
+    function isArrayLike(obj) {
+        var type = typeof obj;
+        if (type === 'string') {
+            return true;
+        } else if (type === 'number') {
+            return false;
+        }
+        var length = obj && type !== 'boolean' && typeof obj !== 'number' && 'length' in obj && obj.length;
+        return typeof obj !== 'function' && (length === 0 || typeof length === 'number' && length > 0 && length - 1 in obj);
+    }
+    module.exports = isArrayLike;
+});
