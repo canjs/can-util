801ee51e72ade661fdb94461e623a8fb552ae18b
Kevin Phillips <kevin@bitovi.com>
2017-06-14 16:36:23 -0500

    3.9.0-pre.7

---
+++ dist/amd/js/is-plain-object/is-plain-object.js
@@ -0,0 +1,25 @@
+/*can-util@3.9.0-pre.6#js/is-plain-object/is-plain-object*/
+define(function (require, exports, module) {
+    'use strict';
+    var core_hasOwn = Object.prototype.hasOwnProperty;
+    function isWindow(obj) {
+        return obj !== null && obj == obj.window;
+    }
+    function isPlainObject(obj) {
+        if (!obj || typeof obj !== 'object' || obj.nodeType || isWindow(obj) || obj.constructor && obj.constructor.name !== 'Object') {
+            return false;
+        }
+        try {
+            if (obj.constructor && !core_hasOwn.call(obj, 'constructor') && !core_hasOwn.call(obj.constructor.prototype, 'isPrototypeOf')) {
+                return false;
+            }
+        } catch (e) {
+            return false;
+        }
+        var key;
+        for (key in obj) {
+        }
+        return key === undefined || core_hasOwn.call(obj, key);
+    }
+    module.exports = isPlainObject;
+});
