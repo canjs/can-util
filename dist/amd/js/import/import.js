5f89a9ae69c3369eff5cb32d6a9aef7fd99c8a4d
Kevin Phillips <kevin@bitovi.com>
2017-06-16 16:37:41 -0500

    3.9.0-pre.8

---
+++ dist/amd/js/import/import.js
@@ -0,0 +1,29 @@
+/*can-util@3.9.0-pre.7#js/import/import*/
+define(function (require, exports, module) {
+    (function (global) {
+        'use strict';
+        var isFunction = require('../is-function/is-function');
+        var global = require('../global/global')();
+        module.exports = function (moduleName, parentName) {
+            return new Promise(function (resolve, reject) {
+                try {
+                    if (typeof global.System === 'object' && isFunction(global.System['import'])) {
+                        global.System['import'](moduleName, { name: parentName }).then(resolve, reject);
+                    } else if (global.define && global.define.amd) {
+                        global.require([moduleName], function (value) {
+                            resolve(value);
+                        });
+                    } else if (global.require) {
+                        resolve(global.require(moduleName));
+                    } else {
+                        resolve();
+                    }
+                } catch (err) {
+                    reject(err);
+                }
+            });
+        };
+    }(function () {
+        return this;
+    }()));
+});
