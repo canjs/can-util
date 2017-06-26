21050f9bebf7c84aae51114342df7a7f558aade0
Chris Andrejewski <christopher.andrejewski@gmail.com>
2017-06-26 13:01:06 -0400

    3.9.1

---
+++ dist/amd/js/get/get.js
@@ -0,0 +1,18 @@
+/*can-util@3.9.0#js/get/get*/
+define(function (require, exports, module) {
+    'use strict';
+    var isContainer = require('../is-container/is-container');
+    function get(obj, name) {
+        var parts = typeof name !== 'undefined' ? (name + '').replace(/\[/g, '.').replace(/]/g, '').split('.') : [], length = parts.length, current, i, container;
+        if (!length) {
+            return obj;
+        }
+        current = obj;
+        for (i = 0; i < length && isContainer(current); i++) {
+            container = current;
+            current = container[parts[i]];
+        }
+        return current;
+    }
+    module.exports = get;
+});
---
+++ dist/amd/js/is-iterable/is-iterable.js
@@ -0,0 +1,8 @@
+/*can-util@3.9.0#js/is-iterable/is-iterable*/
+define(function (require, exports, module) {
+    'use strict';
+    var canSymbol = require('can-symbol');
+    module.exports = function (obj) {
+        return obj && !!obj[canSymbol.iterator || canSymbol.for('iterator')];
+    };
+});
