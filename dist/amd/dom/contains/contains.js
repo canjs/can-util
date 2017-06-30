ed16842b50de12d4ff462ee4dc19f0e75b9f44d6
Christopher Baker <christopher@bitovi.com>
2017-06-30 15:23:02 -0700

    3.9.4

---
+++ dist/amd/dom/contains/contains.js
@@ -0,0 +1,7 @@
+/*can-util@3.9.3#dom/contains/contains*/
+define(function (require, exports, module) {
+    'use strict';
+    module.exports = function (child) {
+        return this.contains(child);
+    };
+});
---
+++ dist/amd/js/is-node/is-node.js
@@ -0,0 +1,11 @@
+/*can-util@3.9.3#js/is-node/is-node*/
+define(function (require, exports, module) {
+    (function (global) {
+        'use strict';
+        module.exports = function () {
+            return typeof process === 'object' && {}.toString.call(process) === '[object process]';
+        };
+    }(function () {
+        return this;
+    }()));
+});
