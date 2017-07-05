edff25e420595ad16681269f913cfcfbc4a332bf
cherif BOUCHELAGHEM <cherif.bouchelaghem@gmail.com>
2017-07-05 17:58:13 +0100

    new branch for assign enhancement

--- js/assign/assign-test.js
+++ js/assign/assign-test.js
@@ -15,3 +15,14 @@ QUnit.test("Assign all properties to an object", function(){
 		equal(expected[prop], actual[prop]);
 	}
 });
+
+QUnit.test("Assign with multiple suppliers as arguments", function(){
+	var a = {a: 1, b: 2, d: 3};
+	var b = {a: 1, b: 3, c: 2};
+	var expected =  {a: 1, b: 3, c: 2, d: 3};
+	var actual = assign({}, a, b);
+
+	for (var prop in actual){
+		equal(expected[prop], actual[prop]);
+	}
+});
