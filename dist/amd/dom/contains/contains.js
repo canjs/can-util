c0b76c8f737e1791cb92f7a0c007cf24e6edfd68
bgando <gandolfo.bianca@gmail.com>
2017-09-06 12:33:33 -0400

    3.10.2

---
+++ dist/amd/dom/contains/contains.js
@@ -0,0 +1,7 @@
+/*can-util@3.10.1#dom/contains/contains*/
+define(function (require, exports, module) {
+    'use strict';
+    module.exports = function (child) {
+        return this.contains(child);
+    };
+});
---
+++ dist/amd/js/is-web-worker/is-web-worker.js
@@ -0,0 +1,11 @@
+/*can-util@3.10.1#js/is-web-worker/is-web-worker*/
+define(function (require, exports, module) {
+    (function (global) {
+        'use strict';
+        module.exports = function () {
+            return typeof WorkerGlobalScope !== 'undefined' && this instanceof WorkerGlobalScope;
+        };
+    }(function () {
+        return this;
+    }()));
+});
