31570d13abe68cb0057e3de170259a644fff0467
imaustink <austin@austinkurpuis.com>
2017-09-06 12:09:43 -0700

    3.10.5

---
+++ dist/amd/dom/document/document.js
@@ -0,0 +1,16 @@
+/*can-util@3.10.4#dom/document/document*/
+define([
+    'require',
+    'exports',
+    'module',
+    '../../js/dev/dev',
+    'can-globals/document'
+], function (require, exports, module) {
+    (function (global) {
+        'use strict';
+        var canDev = require('../../js/dev/dev');
+        module.exports = require('can-globals/document');
+    }(function () {
+        return this;
+    }()));
+});
