4b975b263d9cae936dc920274b7dc0d7b20d5c86
imaustink <austin@austinkurpuis.com>
2017-09-06 10:51:43 -0700

    Update dist for release

---
+++ dist/amd/js/global/global.js
@@ -0,0 +1,16 @@
+/*can-util@3.10.4#js/global/global*/
+define([
+    'require',
+    'exports',
+    'module',
+    '../dev/dev',
+    'can-globals/global'
+], function (require, exports, module) {
+    (function (global) {
+        'use strict';
+        var canDev = require('../dev/dev');
+        module.exports = require('can-globals/global');
+    }(function () {
+        return this;
+    }()));
+});
---
+++ dist/amd/js/is-string/is-string.js
@@ -0,0 +1,14 @@
+/*can-util@3.10.4#js/is-string/is-string*/
+define([
+    'require',
+    'exports',
+    'module',
+    '../dev/dev'
+], function (require, exports, module) {
+    'use strict';
+    var dev = require('../dev/dev');
+    var hasWarned = false;
+    module.exports = function isString(obj) {
+        return typeof obj === 'string';
+    };
+});
