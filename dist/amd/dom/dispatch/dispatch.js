e2403074ce97d70857baff6fa76c5244bcd10c0d
imaustink <austin@austinkurpuis.com>
2017-09-06 17:52:15 -0700

    3.10.6

---
+++ dist/amd/dom/dispatch/dispatch.js
@@ -0,0 +1,13 @@
+/*can-util@3.10.5#dom/dispatch/dispatch*/
+define([
+    'require',
+    'exports',
+    'module',
+    '../events/events'
+], function (require, exports, module) {
+    'use strict';
+    var domEvents = require('../events/events');
+    module.exports = function () {
+        return domEvents.dispatch.apply(this, arguments);
+    };
+});
