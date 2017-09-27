01fd613c53d7208d206449e24b3523e2df3a559d
imasustink <austin@austinkurpuis.com>
2017-09-26 17:48:45 -0700

    Deprecate dom/data/core in favor of can-dom-data-state

--- dom/data/data-test.js
+++ dom/data/data-test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var domData = require("./data");
-var domDataCore = require("./core");
+var domDataState = require("can-dom-data-state");
 var diff = require("../../js/diff-object/diff-object");
 var getDocument = require('can-globals/document/document');
 var mutate = require("../mutate/mutate");
@@ -14,18 +14,18 @@ unit.module('can-util/dom/data');
 unit.test('domData should be cleaned up if element is removed from DOM', function (assert) {
 	var done = assert.async();
 	var fixture = document.getElementById('qunit-fixture');
-	var origDataKeys = Object.keys(domDataCore._data);
+	var origDataKeys = Object.keys(domDataState._data);
 
 	var div = document.createElement('div');
 	mutate.appendChild.call(fixture, div);
 	domData.set.call(div, "div-data", { abc: "def" });
-	var newDataKeys = Object.keys(domDataCore._data);
+	var newDataKeys = Object.keys(domDataState._data);
 	assert.ok(diff(origDataKeys, newDataKeys).length > 0, "items added to domData._data for div");
 
 	mutate.removeChild.call(fixture, div);
 
 	var checkRemoved = function() {
-		if (diff(Object.keys(domDataCore._data), origDataKeys).length === 0) {
+		if (diff(Object.keys(domDataState._data), origDataKeys).length === 0) {
 			assert.ok(true, "domData._data returned to initial state");
 			done();
 		}
@@ -40,25 +40,25 @@ unit.test('domData should be cleaned up if element is removed from DOM', functio
 unit.test('domData should be cleaned up if multiple elements are removed from DOM', function (assert) {
 	var done = assert.async();
 	var fixture = document.getElementById('qunit-fixture');
-	var origDataKeys = Object.keys(domDataCore._data);
+	var origDataKeys = Object.keys(domDataState._data);
 
 	var div = document.createElement('div');
 	mutate.appendChild.call(fixture, div);
 	domData.set.call(div, "div-data", { abc: "def" });
-	var newDataKeys = Object.keys(domDataCore._data);
+	var newDataKeys = Object.keys(domDataState._data);
 	assert.ok(diff(origDataKeys, newDataKeys).length > 0, "items added to domData._data for div");
 
 	var p = document.createElement('p');
 	mutate.appendChild.call(fixture, p);
 	domData.set.call(p, "p-data", { ghi: "jkl" });
-	newDataKeys = Object.keys(domDataCore._data);
+	newDataKeys = Object.keys(domDataState._data);
 	assert.ok(diff(origDataKeys, newDataKeys).length > 1, "items added to domData._data for p");
 
 	mutate.removeChild.call(fixture, div);
 	mutate.removeChild.call(fixture, p);
 
 	var checkRemoved = function() {
-		if (diff(Object.keys(domDataCore._data), origDataKeys).length === 0) {
+		if (diff(Object.keys(domDataState._data), origDataKeys).length === 0) {
 			assert.ok(true, "domData._data returned to initial state");
 			done();
 		}
@@ -73,19 +73,19 @@ unit.test('domData should be cleaned up if multiple elements are removed from DO
 unit.test('domData should be cleaned up if element is removed from DOM after calling setData for two different keys', function (assert) {
 	var fixture = document.getElementById('qunit-fixture');
 	var done = assert.async();
-	var origDataKeys = Object.keys(domDataCore._data);
+	var origDataKeys = Object.keys(domDataState._data);
 
 	var div = document.createElement('div');
 	mutate.appendChild.call(fixture, div);
 	domData.set.call(div, "div-data", { abc: "def" });
 	domData.set.call(div, "div-other-data", { ghi: "jkl" });
-	var newDataKeys = Object.keys(domDataCore._data);
+	var newDataKeys = Object.keys(domDataState._data);
 	assert.ok(diff(origDataKeys, newDataKeys).length > 0, "items added to domData._data for div");
 
 	mutate.removeChild.call(fixture, div);
 
 	var checkRemoved = function() {
-		if (diff(Object.keys(domDataCore._data), origDataKeys).length === 0) {
+		if (diff(Object.keys(domDataState._data), origDataKeys).length === 0) {
 			assert.ok(true, "domData._data returned to initial state");
 			done();
 		}
@@ -100,19 +100,19 @@ unit.test('domData should be cleaned up if element is removed from DOM after cal
 unit.test('domData should be cleaned up if element is removed from DOM after calling setData twice for the same key', function (assert) {
 	var fixture = document.getElementById('qunit-fixture');
 	var done = assert.async();
-	var origDataKeys = Object.keys(domDataCore._data);
+	var origDataKeys = Object.keys(domDataState._data);
 
 	var div = document.createElement('div');
 	mutate.appendChild.call(fixture, div);
 	domData.set.call(div, "div-data", { abc: "def" });
 	domData.set.call(div, "div-data", { ghi: "jkl" });
-	var newDataKeys = Object.keys(domDataCore._data);
+	var newDataKeys = Object.keys(domDataState._data);
 	assert.ok(diff(origDataKeys, newDataKeys).length > 0, "items added to domData._data for div");
 
 	mutate.removeChild.call(fixture, div);
 
 	var checkRemoved = function() {
-		if (diff(Object.keys(domDataCore._data), origDataKeys).length === 0) {
+		if (diff(Object.keys(domDataState._data), origDataKeys).length === 0) {
 			assert.ok(true, "domData._data returned to initial state");
 			done();
 		}
