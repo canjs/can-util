5831e1972006af51782313e19dd6d677a476e3bb
imasustink <austin@austinkurpuis.com>
2017-10-02 19:27:24 -0700

    3.10.10

---
+++ dist/amd/dom/ajax/ajax.js
@@ -0,0 +1,12 @@
+/*can-util@3.10.9#dom/ajax/ajax*/
+define([
+    'require',
+    'exports',
+    'module',
+    '../../js/dev/dev',
+    'can-ajax'
+], function (require, exports, module) {
+    'use strict';
+    var canDev = require('../../js/dev/dev');
+    module.exports = require('can-ajax');
+});
---
+++ dist/amd/js/set-immediate/set-immediate.js
@@ -0,0 +1,17 @@
+/*can-util@3.10.9#js/set-immediate/set-immediate*/
+define([
+    'require',
+    'exports',
+    'module',
+    'can-globals/global'
+], function (require, exports, module) {
+    (function (global) {
+        'use strict';
+        var global = require('can-globals/global')();
+        module.exports = global.setImmediate || function (cb) {
+            return setTimeout(cb, 0);
+        };
+    }(function () {
+        return this;
+    }()));
+});
