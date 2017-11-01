b2bc7db5946541a3515b2532b639b0b08ba60cda
imasustink <austin@austinkurpuis.com>
2017-11-01 14:48:43 -0700

    3.10.13

---
+++ dist/amd/js/is-promise/is-promise.js
@@ -0,0 +1,13 @@
+/*can-util@3.10.12#js/is-promise/is-promise*/
+define([
+    'require',
+    'exports',
+    'module',
+    'can-reflect'
+], function (require, exports, module) {
+    'use strict';
+    var canReflect = require('can-reflect');
+    module.exports = function (obj) {
+        return canReflect.isPromise(obj);
+    };
+});
