21add0f576d2be414e2e4b38757786ebee913de5
Chris Andrejewski <christopher.andrejewski@gmail.com>
2018-01-11 12:51:22 -0500

    3.11.0

---
+++ dist/amd/dom/ajax/ajax.js
@@ -0,0 +1,12 @@
+/*can-util@3.10.19#dom/ajax/ajax*/
+define([
+    'require',
+    'exports',
+    'module',
+    'can-log/dev',
+    'can-ajax'
+], function (require, exports, module) {
+    'use strict';
+    var canDev = require('can-log/dev');
+    module.exports = require('can-ajax');
+});
---
+++ dist/amd/js/is-node/is-node.js
@@ -0,0 +1,11 @@
+/*can-util@3.10.19#js/is-node/is-node*/
+define(function (require, exports, module) {
+    (function (global, require, exports, module) {
+        'use strict';
+        module.exports = function () {
+            return typeof process === 'object' && {}.toString.call(process) === '[object process]';
+        };
+    }(function () {
+        return this;
+    }(), require, exports, module));
+});
