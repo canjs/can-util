078d1a0c3ab9ecc03cb51aa4458b6d5715fbcc2b
Christopher Baker <christopher@bitovi.com>
2018-02-16 11:33:21 -0800

    3.11.3

---
+++ dist/amd/js/global/global.js
@@ -0,0 +1,14 @@
+/*can-util@3.11.2#js/global/global*/
+define([
+    'require',
+    'exports',
+    'module',
+    'can-globals/global'
+], function (require, exports, module) {
+    (function (global, require, exports, module) {
+        'use strict';
+        module.exports = require('can-globals/global');
+    }(function () {
+        return this;
+    }(), require, exports, module));
+});
---
+++ dist/amd/js/join-uris/join-uris.js
@@ -0,0 +1,26 @@
+/*can-util@3.11.2#js/join-uris/join-uris*/
+define([
+    'require',
+    'exports',
+    'module',
+    'can-parse-uri'
+], function (require, exports, module) {
+    'use strict';
+    var parseURI = require('can-parse-uri');
+    module.exports = function (base, href) {
+        function removeDotSegments(input) {
+            var output = [];
+            input.replace(/^(\.\.?(\/|$))+/, '').replace(/\/(\.(\/|$))+/g, '/').replace(/\/\.\.$/, '/../').replace(/\/?[^\/]*/g, function (p) {
+                if (p === '/..') {
+                    output.pop();
+                } else {
+                    output.push(p);
+                }
+            });
+            return output.join('').replace(/^\//, input.charAt(0) === '/' ? '/' : '');
+        }
+        href = parseURI(href || '');
+        base = parseURI(base || '');
+        return !href || !base ? null : (href.protocol || base.protocol) + (href.protocol || href.authority ? href.authority : base.authority) + removeDotSegments(href.protocol || href.authority || href.pathname.charAt(0) === '/' ? href.pathname : href.pathname ? (base.authority && !base.pathname ? '/' : '') + base.pathname.slice(0, base.pathname.lastIndexOf('/') + 1) + href.pathname : base.pathname) + (href.protocol || href.authority || href.pathname ? href.search : href.search || base.search) + href.hash;
+    };
+});
