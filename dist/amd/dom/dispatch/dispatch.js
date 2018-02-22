35e3d96513aa490b2ee4a77c50e780d729df75bc
Chasen Le Hara <chasen@chasenlehara.com>
2018-02-22 09:08:43 -0800

    3.11.4

---
+++ dist/amd/dom/dispatch/dispatch.js
@@ -0,0 +1,13 @@
+/*can-util@3.11.3#dom/dispatch/dispatch*/
+define([
+    'require',
+    'exports',
+    'module',
+    '../events/events'
+], function (require, exports, module) {
+    'use strict';
+    var domEvents = require('../events/events');
+    module.exports = function () {
+        return domEvents.dispatch.apply(this, arguments);
+    };
+});
